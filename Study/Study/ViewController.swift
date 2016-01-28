15e8be92d3123faa38a5e55e7b519c2183e51479
Róbert Tarabčák <tarabcakr@gmail.com>
2016-01-28 12:46:52 +0100

    Initial commit

---
+++ Study/Study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Study
+//
+//  Created by Robert Tarabcak on 28.01.16.
+//  Copyright © 2016 Robert Tarabcak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
